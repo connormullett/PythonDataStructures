2d669daea0ecc778be6e181a5cafe34e98cd6446
connormullett <connormullett@gmail.com>
2019-03-13 09:26:01 -0400

    initial scaffolding, working on linked_list

---
+++ stacks/linked_list.py
@@ -0,0 +1,28 @@
+
+
+class Node:
+
+    def __init__(self, data, next):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+
+    def __call__(self):
+        return LinkedList()
+
+    def createList(self):
+        pass
+
+    def deleteList(self):
+        pass
+
+    def insert(self):
+        pass
+
+    def search(self):
+        pass
