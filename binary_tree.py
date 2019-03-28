1c930ca3254824273077ef97cf43c16697d29792
Connor <connormullett@gmail.com>
2019-03-28 02:46:00 +0000

    scaffolding search tree

--- binary_tree.py
+++ binary_tree.py
@@ -1,1 +1,24 @@
 
+# simple binary search tree that supports traversal, insertion, and deletion
+# uses no external libraries
+
+
+class Node(object):
+
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinaryTree(object):
+
+    def insert():
+        pass
+
+    def search(self):
+        pass
+
+    def delete(self):
+        pass
+
